5d8b4bf10be991b4f3e3443ae2c1abe4296c0a39
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-11 21:00:47 +0200

    polish styling of section features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,14 +87,19 @@ const Home: NextPage = () => {
             <p className="mx-auto mt-3 text-left text-xl text-neutral-100 lg:mt-6 lg:max-w-7xl lg:text-center">
               Find a recipe you will love. You can add it to your favorites too!
             </p>
-            <Tabs defaultValue="recipes" className="mt-12 lg:mt-3 rounded-lg lg:p-10">
+            <Tabs
+              defaultValue="recipes"
+              className="mt-12 rounded-lg lg:mt-3 lg:p-10"
+            >
               <TabsList>
-                <TabsTrigger value="recipes">Recipes</TabsTrigger>
+                <TabsTrigger value="recipes" className="bg-white/10">
+                  Recipes
+                </TabsTrigger>
                 <TabsTrigger value="favorites">Favorites</TabsTrigger>
               </TabsList>
               <TabsContent
                 value="recipes"
-                className="mt-6 border border-white/25"
+                className="mt-6 border border-white/25 bg-white/10 lg:p-16"
               >
                 <p className="mb-6 w-full text-left text-sm text-neutral-200 lg:mx-auto lg:max-w-md lg:text-base">
                   Browse through recipes and find your favorite one! Clicking
@@ -113,7 +118,7 @@ const Home: NextPage = () => {
               </TabsContent>
               <TabsContent
                 value="favorites"
-                className="mt-6 border border-white/25"
+                className="mt-6 border border-white/25 bg-white/10 lg:p-16"
               >
                 <p className="mb-6 w-full text-left text-sm text-neutral-200 lg:mx-auto lg:max-w-md lg:text-base">
                   This is the place where you can see all the recipes you
