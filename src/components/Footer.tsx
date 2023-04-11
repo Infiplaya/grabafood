ed23402ed1aa4eb8f3c628f14f7d638ea099f75d
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-11 17:43:15 +0200

    change footer border color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="w-full  border-t-2 border-neutral-300  py-16 text-neutral-600">
+    <footer className="w-full  border-t-2 border-neutral-200  py-16 text-neutral-600">
       <div className="flex w-full justify-between lg:mx-auto lg:max-w-7xl">
         <ul className="px-10 lg:space-y-5">
           <li>
