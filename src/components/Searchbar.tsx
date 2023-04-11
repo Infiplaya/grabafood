1fb9a139249dfda6a2f77c18e71c5bd2c25542b6
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-11 18:13:23 +0200

    add form to searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,12 +1,23 @@
 import { SearchIcon } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 import { Input } from "./ui/Input";
 
 export function Searchbar() {
   const [query, setQuery] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    void router.push({
+      pathname: "/recipes",
+      query: { q: query.toLowerCase() },
+    });
+  };
   return (
-    <div className="relative mt-6 w-full">
+    <form className="relative mt-6 w-full" onSubmit={handleSubmit}>
       <Input
         type="search"
         value={query}
@@ -14,14 +25,9 @@ export function Searchbar() {
         placeholder="Search a food..."
         className="w-full flex-1 rounded-3xl px-7 py-7 text-base"
       />
-      <Link
-        href={{
-          pathname: "/recipes",
-          query: { q: query },
-        }}
-      >
+      <button type="submit">
         <SearchIcon className="absolute right-4 top-3 h-8 w-8 rounded-full bg-orange-500 p-1 text-neutral-200 hover:bg-orange-400" />
-      </Link>
-    </div>
+      </button>
+    </form>
   );
 }
