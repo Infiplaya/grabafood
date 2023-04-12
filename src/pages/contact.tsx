9a91e3b213bb333d32277435a14a930512b1e174
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-12 20:16:02 +0200

    fix layout of contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,20 +16,22 @@ const Contact: NextPage = () => {
       </Head>
 
       <Layout>
-        <h1 className="text-7xl">Contact me</h1>
-        <ul className="mt-10 text-4xl">
-          <li>
-            <Link href={`mailto:pawelsobaniec6@gmail.com`}>Email</Link>
-          </li>
-          <li>
-            <Link href={`https://github.com/Infiplaya`}>Github</Link>
-          </li>
-          <li>
-            <Link href={`https://www.linkedin.com/in/psobaniec09/`}>
-              Linkedin
-            </Link>
-          </li>
-        </ul>
+        <div className="pb-64 pt-32 lg:px-36">
+          <h1 className="text-7xl">Contact me</h1>
+          <ul className="mt-10 text-4xl">
+            <li>
+              <Link href={`mailto:pawelsobaniec6@gmail.com`}>Email</Link>
+            </li>
+            <li>
+              <Link href={`https://github.com/Infiplaya`}>Github</Link>
+            </li>
+            <li>
+              <Link href={`https://www.linkedin.com/in/psobaniec09/`}>
+                Linkedin
+              </Link>
+            </li>
+          </ul>
+        </div>
       </Layout>
     </>
   );
