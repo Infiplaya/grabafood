92aa2536a20b33778f38668da74a912203ace459
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-13 21:59:43 +0200

    add padding bottom

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from "react";
 
 export function Layout({ children }: { children: ReactNode }) {
-  return <main className="mt-12 px-6 lg:mx-auto lg:max-w-7xl">{children}</main>;
+  return <main className="mt-12 px-6 lg:mx-auto lg:max-w-7xl pb-32">{children}</main>;
 }
