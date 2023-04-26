e1d187ad8ee40489d741966848f79f7d63b9e31a
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-26 17:41:17 +0200

    fix bug with search query

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -21,7 +21,7 @@ export async function getStaticProps() {
   };
 }
 
-const querySchema = z.string().min(1).optional();
+const querySchema = z.string().optional();
 
 export default function Recipes({
   recipes,
