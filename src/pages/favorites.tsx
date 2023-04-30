012ab39955bfa5d8cc09225fbba5db09c3dcb01b
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-30 19:31:31 +0200

    make button work

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/Button";
 import { Layout } from "@/components/ui/Layout";
 import { useRemoveFromFavorites } from "@/hooks/useDeleteFromFavorites";
 import { api } from "@/utils/api";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Favorites() {
@@ -27,7 +27,7 @@ export default function Favorites() {
   return (
     <Layout>
       {session ? (
-        <div className="min-h-[800px] space-y-5 mb-12 rounded-lg bg-white px-6 py-12 shadow-md">
+        <div className="mb-12 min-h-[800px] space-y-5 rounded-lg bg-white px-6 py-12 shadow-md">
           {data?.favoriteRecipes.map((recipe) => (
             <div
               className="flex items-center justify-between gap-5 border-b-2 border-neutral-100 px-10 py-2"
@@ -54,7 +54,10 @@ export default function Favorites() {
       ) : (
         <div className="-mt-32 flex min-h-screen w-full items-center justify-center">
           <p className="flex items-center text-xl font-medium lg:text-4xl">
-            <Button className="mr-3 py-6 text-xl lg:text-4xl" variant="subtle">
+            <Button
+              className="mr-3 py-6 text-xl lg:text-4xl"
+              onClick={() => void signIn()}
+            >
               Sign In{" "}
             </Button>
             to see your favorites!
